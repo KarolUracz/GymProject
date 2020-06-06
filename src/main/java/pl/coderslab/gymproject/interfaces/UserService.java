f7ff43139a999ed8e883bc6f6e938bad742a8add
Karol Uracz <karol_ur@interia.pl>
2020-06-06 10:32:35 +0200

    user controller code refactor

--- src/main/java/pl/coderslab/gymproject/interfaces/UserService.java
+++ src/main/java/pl/coderslab/gymproject/interfaces/UserService.java
@@ -1,6 +1,7 @@
 package pl.coderslab.gymproject.interfaces;
 
 import pl.coderslab.gymproject.Model.CurrentUser;
+import pl.coderslab.gymproject.entity.Pass;
 import pl.coderslab.gymproject.entity.User;
 
 import java.util.List;
@@ -8,22 +9,15 @@ import java.util.List;
 public interface UserService {
 
     User findByUserName(String name);
-
     void saveUser(User user);
-
     List<User> findAll();
-
     User findById(long id);
-
     void updateUser(User user);
-
     void delete(long id);
-
     void saveAdmin(User user);
-
     void saveTrainer(User user);
-
     List<User> findAllByRolesN_NameLike(String role);
-
     void changePassword(CurrentUser currentUser, User user);
+    void extendPass(CurrentUser currentUser, long passId);
+    Pass getPass(CurrentUser currentUser, long passId);
 }
